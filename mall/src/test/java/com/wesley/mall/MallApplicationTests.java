cfd4bf4681d76e1ba99a8969b2f13108d0ec3469
wesley <tgfc040@tgfc.tw>
2019-10-18 14:17:59 +0800

    first commit

---
+++ mall/src/test/java/com/wesley/mall/MallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesley.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
